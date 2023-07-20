d4b8eb12d81fb3f0e1e82e107910b8f1f601b83b
Ivo Dator <11011787+ivodator@users.noreply.github.com>
2023-07-20 13:34:57 -0700

    Update Footer.js
    Removed footer to make it look cleaner

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,1 @@
-import styles from './Footer.module.css'
 
-export default function Footer() {
-  return (
-    <>
-        <p className="footer">
-          Contact
-        </p>
-      </footer>
-    </>
-  )
-}
