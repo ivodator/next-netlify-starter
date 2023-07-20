c4dd2762d4bc01ce2fbedc5aa71307373363682e
Ivo Dator <11011787+ivodator@users.noreply.github.com>
2023-07-20 13:32:30 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <p className="footer">
+          Contact
+        </p>
       </footer>
     </>
   )
